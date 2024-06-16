5726eaf97e186f22ccab0a8676d9ab56d6016596
Homer Lin <125667301+Homer-Lin@users.noreply.github.com>
2024-06-16 14:24:05 +0800

    Add files via upload

---
+++ Week03/homework02.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自傳</title>
+</head>
+<body>
+    <h1>我的自傳</h1>
+    <div class="content">
+        <img src="高大-林建宏.jpg" alt="我的照片" class="profile-pic" width="10%" height="auto">
+        <p>我來自彰化縣，雖然無聊但卻有很多好吃的，下次來彰化可以來找我喔</p>
+
+        <hr>
+
+        <h2>學歷</h2>
+        <table border=5px>
+            <tr>
+                <th>學校</th>
+                <th>學位</th>
+                <th>年份</th>
+            </tr>
+            <tr>
+                <td>彰化高中</td>
+                <td>畢業</td>
+                <td>2019 - 2022</td>
+            </tr>
+            <tr>
+                <td>國立高雄大學</td>
+                <td>資訊管理學系</td>
+                <td>2022 - 2026</td>
+            </tr>
+        </table>
+
+        <hr>
+
+        <h2>經歷</h2>
+        <table border=5px>
+            <tr>
+                <th>單位</th>
+                <th>身分</th>
+                <th>年份</th>
+            </tr>
+            <tr>
+                <td>104人力銀行</td>
+                <td>校園經理</td>
+                <td>2023.7-2024.6</td>
+            </tr>
+            <tr>
+                <td>高大資管系</td>
+                <td>系學會會長</td>
+                <td>2024.6-2025.5</td>
+            </tr>
+        </table>
+
+        <hr>
+
+        <h2>興趣與愛好</h2>
+        <p>喜歡聽各式各樣的音樂，歡迎來找我去KTV</p>
+
+        <hr>
+
+        <h2>專長</h2>
+        <ul>
+            <li>程式設計</li>
+            <li>網頁開發</li>
+            <li>文書處理</li>
+            <li>口語表達</li>
+            <li>簡報敘事</li>
+        </ul>
+
+        <hr>
+
+        <h2>聯絡方式</h2>
+        <p>您可以通過以下方式聯絡我：</p>
+        <ul>
+            <li>Email: <a href="mailto:a1113329@mail.nuk.edu.tw" target="_blank">a1113329@mail.nuk.edu.tw</a></li>
+            <li>FB: <a href="https://www.facebook.com/profile.php?id=100006554504226" target="_blank">林建宏</a></li>
+            <li>IG: <a href="https://www.instagram.com/homer_105216/" target="_blank">homer_105216</a></li>
+        </ul>
+    </div>
+</body>
+</html>
